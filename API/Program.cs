79ceef91da965448928490fa1dc2250df26893d6
Ramón Juárez <ramon.juarez@paysafe.com>
2023-04-21 10:16:43 -0600

    Second Module

--- API/Program.cs
+++ API/Program.cs
@@ -11,6 +11,9 @@ builder.Services.AddDbContext<DataContext>(opt => {
     
 });
 
+builder.Services.AddCors();
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -27,6 +30,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors(builder=> builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.MapControllers();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'Dating app';
+  users : any;
+
+  
+  constructor(private http: HttpClient)
+  {
+  }
+  
+  ngOnInit(){
+    this.users = this.http.get("https://localhost:5001/api/users").subscribe({
+      next: response => this.users = response,
+      error: () => {},
+      complete:  () => {},
+    });
+
+    console.log(this.users);
+  }
+
+
+  
+
+
+
+}
