f51c9c07b155bc7f6310204dadf571cef3c475de
Ramón Juárez <ramon.juarez@paysafe.com>
2023-04-27 10:23:43 -0600

    Fourth Module

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
 
 @Component({
   selector: 'app-root',
@@ -10,24 +11,19 @@ export class AppComponent implements OnInit {
   title = 'Dating app';
   users : any;
 
-  
-  constructor(private http: HttpClient)
+  constructor(private accountService: AccountService)
   {
   }
   
   ngOnInit(){
-    this.users = this.http.get("https://localhost:5001/api/users").subscribe({
-      next: response => this.users = response,
-      error: () => {},
-      complete:  () => {},
-    });
-
-    console.log(this.users);
+   this.setCurrentUser();
   }
 
-
-  
-
-
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return ;
+    const user : User =  JSON.parse(userString);
+    this.accountService.setCurrentUser(user);
+  }
 
 }
